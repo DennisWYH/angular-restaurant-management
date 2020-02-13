cc6501f870d9e0a35b20fdbc3356dfad89dc9af9
DennisWYH <wangyh@yeah.net>
2020-02-13 15:14:49 +0100

    create formControl in purchase-form component that can be displayed in purchase component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,29 @@ import { AppComponent } from './app.component';
 import { PurchaseComponent } from './purchase/purchase.component';
 import { RouterModule } from '@angular/router';
 import { Routes} from '@angular/router';
+import { SummaryComponent } from './summary/summary.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { PurchaseFormComponent } from './purchase-form/purchase-form.component';
+
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: PurchaseComponent },
+      { path: 'summary', component: SummaryComponent },
     ])
   ],
   
   declarations: [
     AppComponent,
-    PurchaseComponent
+    PurchaseComponent,
+    SummaryComponent,
+    PurchaseFormComponent,
   ],
   bootstrap: [ AppComponent ]
 })
