69cb54ea1e97d38c5f4a3bcf241241779ed5c461
DennisWYH <wangyh@yeah.net>
2020-02-13 16:02:41 +0100

    add the formControls into the formGroup

--- src/app/purchase-form/purchase-form.component.ts
+++ src/app/purchase-form/purchase-form.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-purchase-form',
   templateUrl: './purchase-form.component.html',
   styleUrls: ['./purchase-form.component.css']
 })
+
 export class PurchaseFormComponent implements OnInit {
   
-  name = new FormControl('');
-  date = new FormControl('');
-  price = new FormControl('');
-  person = new FormControl('');
+  purchaseForm = new FormGroup({
+    name: new FormControl(''),
+    date: new FormControl(''),
+    price: new FormControl(''),
+    person: new FormControl(''),
+  });
+
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSubmit(){
+    console.warn('The form data has been submitted.');
+  }
 }
